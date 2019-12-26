1a562bae6bd07debfadf221ab102ed4a5334f5d0
b0dah <ries8898@gmail.com>
2019-12-26 18:40:04 +0300

    fix state 2

--- resources/public/js/app.js
+++ resources/public/js/app.js
@@ -1,7 +1,7 @@
 window.onload = function() {
   var input = document.getElementById('input');
   var openBtn = document.getElementById('open');
-  var sendBtn = document.getElementById('send');
+//  var sendBtn = document.getElementById('send');
   var closeBtn = document.getElementById('close');
   var messages = document.getElementById('messages');
   
@@ -15,7 +15,7 @@ window.onload = function() {
 
   function output(style, text){
   messages.innerHTML += "<br/><span class='" + style + "'>" + text + "</span>";
-}
+  }
 
     // Open
     openBtn.onclick = function(e) {
@@ -49,15 +49,27 @@ window.onload = function() {
     };
 
     // Send
-    sendBtn.onclick = function(e) {
-        if (socket == undefined) {
-            output("error", 'Not connected');
-            return;
-        }
-        var text = document.getElementById("input").value;
-        socket.send(text);
-        output("sent", ">>> " + text);
+//    sendBtn.onclick = function(e) {
+//        if (socket == undefined) {
+//            output("error", 'Not connected');
+//            return;
+//        }
+//        var text = document.getElementById("input").value;
+//        socket.send(text);
+//        output("sent", ">>> " + text);
+//    };    
+    function sendCommand() {
+      if (socket == undefined) {
+        output("error", 'Not connected');
+        return;
+      }
+      var text = document.getElementById("input").value;
+      socket.send(text);
+      input.value = '';
+      output("sent", ">>> " + text);
     };
+    
+//    sendBtn.onclick = sendCommand();
 
     // Close
     closeBtn.onclick = function(e) {
@@ -71,16 +83,16 @@ window.onload = function() {
     // Movement Directions
     northButton.onclick = function (e) {
       moveToGivenDirection("north")
-    }
+    };
     westButton.onclick = function (e) {
       moveToGivenDirection("west")
-    }
+    };
     eastButton.onclick = function (e) {
       moveToGivenDirection("east")
-    }
+    };
     southButton.onclick = function (e) {
-      moveToGivenDirection("north")
-    }
+      moveToGivenDirection("south")
+    };
     
     function moveToGivenDirection(direction) {
       if (socket == undefined) {
@@ -89,8 +101,37 @@ window.onload = function() {
       }
       socket.send(direction);
       output("sent", ">>> " + direction);
-    }
+    };
     
-    
+    document.addEventListener('keyup', (e) => {
+      if (e.code === "Enter") {
+        sendCommand();
+      }
+      
+      if (e.code === "ArrowUp") {
+        moveToGivenDirection("north")
+      }
+      if (e.code === "ArrowLeft") {
+        moveToGivenDirection("west")
+      }
+      if (e.code === "ArrowRight") {
+        moveToGivenDirection("east")
+      }
+      if (e.code === "ArrowDown") {
+        moveToGivenDirection("south")
+      }
+      
+    });
     
 };
+
+// Key Pressed Listeners
+//window.onkeyup = function (e) {
+//  let key = e.keyCode ? e.keyCode : e.which;
+  
+
+//  if (key == 13) {
+//    alert(key);
+//    sendCommand();
+//  }
+//};
